aa9e5d95c2ab86213d867cdbe72d0e70114bc0bc
MohammadQanit <qanit.mohammad@student.greenriver.edu>
2025-04-12 12:56:46 -0700

    Update HelloWorld3.py

--- start/CH02/HelloWorld3.py
+++ start/CH02/HelloWorld3.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python3
 # A more complex "Hello World" script in python with Inputs
 # Created 
+
+your_name = input("what is your name?  ")
+print("Hello {0}".format(your_name))
+print(f"Hello {your+name}")
+Print("hello " + your_name)
+print("Hello", Your_name)
+message = "Hello" + " " + your_name
+Print(message)
